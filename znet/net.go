21cdc0453694cfb90a9b9016ef257e4415c3b1c1
zhanglei <zhanglei@supercxp.com>
2018-03-14 05:23:01 -0400

    add znet

---
+++ znet/net.go
@@ -0,0 +1,31 @@
+package znet
+
+import (
+	"net"
+)
+
+func Iprange(cidr string) ([]string, error) {
+	ip, ipnet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, err
+	}
+
+	var ips []string
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		ips = append(ips, ip.String())
+	}
+	if len(ips) < 3 {
+		return ips[0:len(ips)], nil
+	}
+	// remove broadcast address
+	return ips[1 : len(ips)-1], nil
+}
+
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
