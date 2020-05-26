4fe1a1369942d9ab1620affaf5a31486f127e2a9
zhanglei <zhanglei@supercxp.com>
2020-05-26 13:44:41 +0800

    up

--- ttlevent/cache.go
+++ ttlevent/cache.go
@@ -10,7 +10,7 @@ type Cache struct {
 	pool  map[string]int64
 	mu    sync.RWMutex
 	ttl   int64
-	event chan string
+	Event chan string
 }
 
 //NewCache iniate an object
@@ -18,7 +18,7 @@ func NewCache(ttl int) *Cache {
 	m := new(Cache)
 	m.pool = make(map[string]int64)
 	m.ttl = int64(ttl)
-	m.event = make(chan string, 1000)
+	m.Event = make(chan string, 1000)
 	go m.check()
 	return m
 }
@@ -31,7 +31,7 @@ func (c *Cache) expire(t int64) bool {
 func (c *Cache) Put(key string) {
 	c.mu.Lock()
 	if _, ok := c.pool[key]; !ok {
-		c.event <- "1/" + key
+		c.Event <- "1/" + key
 	}
 	c.pool[key] = time.Now().Unix()
 	c.mu.Unlock()
@@ -43,7 +43,7 @@ func (c *Cache) check() {
 		c.mu.Lock()
 		for k, v := range c.pool {
 			if c.expire(v) {
-				c.event <- "0/" + k
+				c.Event <- "0/" + k
 				delete(c.pool, k)
 			}
 		}
