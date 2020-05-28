0f1fe1fc92d8a340486dc0bc3e4d849c195ccf30
zhanglei <zhanglei@supercxp.com>
2020-05-28 09:55:14 +0800

    add

---
+++ TireTree/prefixTree.go
@@ -0,0 +1,96 @@
+package main
+
+import "fmt"
+
+type Trie struct {
+	path     string
+	parent   *Trie
+	children []*Trie
+}
+
+/** Initialize your data structure here. */
+func Constructor() Trie {
+	t := new(Trie)
+	return *t
+}
+
+func (this *Trie) show() {
+	fmt.Printf("path:%s\n", this.path)
+	for _, v := range this.children {
+		v.show()
+	}
+}
+
+/** Inserts a word into the trie. */
+func (this *Trie) Insert(word string) {
+	if word[0] != '/' {
+		word = "/" + word
+	}
+	pl := len(this.path)
+	wl := len(word)
+	max := pl
+	if wl < max {
+		max = wl
+	}
+	//确定相同前缀位置
+	l := 0
+	for ; l < max && word[l] == this.path[l]; l++ {
+	}
+	//as root
+	if l == 0 {
+		this.path = word
+		return
+	} else if l < pl {
+		//split this node
+		t := new(Trie)
+		t.path = this.path[l:pl]
+		this.path = this.path[:l]
+		this.children = append(this.children, t)
+		if wl > l {
+			nc := new(Trie)
+			nc.path = word[l:wl]
+			this.children = append(this.children, nc)
+		}
+	} else if l < wl {
+		//add as children
+		t := new(Trie)
+		t.path = word[l:wl]
+		this.children = append(this.children, t)
+	} else {
+		//node already exists
+		//drop here
+	}
+
+}
+
+/** Returns if the word is in the trie. */
+func (this *Trie) Search(word string) bool {
+	if word[0] != '/' {
+		word = "/" + word
+	}
+	for this != nil {
+		pl := len(this.path)
+		fmt.Printf("++++----------:%s\n", this.path)
+		if pl < len(word) {
+			if this.path == word[:pl] {
+				for _, v := range this.children {
+					fmt.Printf("++++:%s\n", word[pl:])
+					if v.Search(word[pl:]) {
+						return true
+					}
+				}
+			}
+		} else if pl > len(word) {
+			return false
+		} else if this.path == word {
+			return true
+		}
+		return false
+	}
+	return false
+}
+
+/** Returns if there is any word in the trie that starts with the given prefix. */
+func (this *Trie) StartsWith(prefix string) bool {
+	return false
+}
