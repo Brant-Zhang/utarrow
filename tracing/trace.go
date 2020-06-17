f3c6e4c0eaf67699af6ab0496081a7957fef79cd
zhanglei <zhanglei@supercxp.com>
2020-06-17 11:24:59 +0800

    add

--- tracing/trace.go
+++ tracing/trace.go
@@ -9,21 +9,21 @@ import (
 
 var Gtracer *zipkin.Tracer
 
-func NewZipkin(url string, servicename string, local string) *zipkin.Tracer {
+func InitZipkin(url string, servicename string, local string) error {
 	reporter := reco.NewReporter(url)
 	ep, err := zipkin.NewEndpoint(servicename, local)
 	if err != nil {
-		return nil
+		return err
 	}
 	tracer, err := zipkin.NewTracer(reporter, zipkin.WithLocalEndpoint(ep))
 	if err != nil {
-		return nil
+		return err
 	}
 	Gtracer = tracer
-	return tracer
+	return nil
 }
 
-func NewTraceServer(tags map[string]string) *grpc.Server {
+func NewRpcServer(tags map[string]string) *grpc.Server {
 	h := zipkinrpc.NewServerHandler(Gtracer, zipkinrpc.ServerTags(tags))
 	op := grpc.StatsHandler(h)
 	s := grpc.NewServer(op)
